68f58dc07e6fa54bf5463813649f3fb2b4ee34b8
tahayk3 <tahayk3@gmail.com>
2024-01-11 16:56:27 -0600

    agregando navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import NavbarDefault from './components/NavbarDefault/NavbarDefault';
+import Welcome from './components/welcome/welcome';
+
+
 const App = () => {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-4xl font-bold">¡Bienvenido(a) a mi aplicación con Tailwind!</h1>
-      <p className="text-lg">Explora todas las posibilidades que ofrece Tailwind para tu proyecto de React.</p>
-    </div>
+   <>
+    <NavbarDefault></NavbarDefault>
+    <Welcome></Welcome>
+   </>
   );
 };
 export default App;
---
+++ src/components/welcome/welcome.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import './welcome.css';
+
+export default function Welcome(){
+    return (
+       <div>
+         <h1>HOLA KARIOSA</h1>
+       </div>
+    );
+}
