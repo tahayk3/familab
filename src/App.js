b8fa64cfb14309d0e9b324807f5b745557524ce2
tahayk3 <tahayk3@gmail.com>
2024-01-11 14:38:32 -0600

    agregando tailwind al proyecto

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-4xl font-bold">¡Bienvenido(a) a mi aplicación con Tailwind!</h1>
+      <p className="text-lg">Explora todas las posibilidades que ofrece Tailwind para tu proyecto de React.</p>
+    </div>
+  );
+};
+export default App;
