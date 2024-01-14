2dc91c42e6429e69c81a0b5145c3b40f8169aab3
tahayk3 <tahayk3@gmail.com>
2024-01-13 21:36:23 -0600

    modificando rutas

--- src/components/NavbarDefault/NavbarDefault.jsx
+++ src/components/NavbarDefault/NavbarDefault.jsx
@@ -6,9 +6,9 @@ import './NavbarDefault.css';
 
 const navigation = [
   { name: 'Inicio', href: '/familab/build', current: true },
-  { name: 'Ubicación', href: '/', current: false },
-  { name: '¿Quiénes somos?', href: '/', current: false },
-  { name: 'Contáctenos', href: '', current: false },
+  { name: 'Ubicación', href: '/ubicacion', current: false },
+  { name: '¿Quiénes somos?', href: '/sobre-nosotros', current: false },
+  { name: 'Contáctenos', href: '/contacto', current: false },
   { name: 'Preguntas frecuentes', href: '/preguntas-frecuentes', current: false },
 ]
 
