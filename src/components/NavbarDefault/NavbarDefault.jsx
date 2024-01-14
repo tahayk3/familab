b9d44841777ab5ee6aec2cef3791f2c9e71e17d8
tahayk3 <tahayk3@gmail.com>
2024-01-14 09:59:02 -0600

    modificando navbar

--- src/components/NavbarDefault/NavbarDefault.jsx
+++ src/components/NavbarDefault/NavbarDefault.jsx
@@ -65,6 +65,7 @@ export default function NavbarDefault() {
             </div>
           </div>
 
+
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
