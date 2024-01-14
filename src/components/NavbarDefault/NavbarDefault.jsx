ea05c3efdfc4050e47459167f830afb861ae28fc
tahayk3 <tahayk3@gmail.com>
2024-01-13 22:45:52 -0600

    modificando rutas, finales 5

--- src/components/NavbarDefault/NavbarDefault.jsx
+++ src/components/NavbarDefault/NavbarDefault.jsx
@@ -4,7 +4,6 @@ import miImagen from '../welcome/logo.png';
 import './NavbarDefault.css';
 import { Link } from 'react-router-dom';
 
-
 const navigation = [
   { name: 'Inicio', href: '/familab/build', current: true },
   { name: 'Ubicación', href: '/ubicacion', current: false },
@@ -65,20 +64,18 @@ export default function NavbarDefault() {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+            <div className="space-y-1 px-2 pb-3 pt-1">
               {navigation.map((item) => (
-                <Link
-                to={item.href}
-                className={classNames(
-                  item.current ? 'bg-[#ff5325] text-white' : 'text-white-100 hover:bg-[#ff5325] hover:text-white',
-                  'rounded-md px-3 py-2 text-sm font-medium'
-                )}
-                aria-current={item.current ? 'page' : undefined}
-              >
-                {item.name}
-                
-
-                  </Link>
+                 <Link
+                 to={item.href}
+                 className={classNames(
+                   item.current ? 'bg-[#ff5325] text-white' : 'text-white-100 hover:bg-[#ff5325] hover:text-white',
+                   'rounded-md px-3 py-2 text-sm font-medium'
+                 )}
+                 aria-current={item.current ? 'page' : undefined}
+               >
+                 {item.name}
+               </Link>
               ))}
             </div>
           </Disclosure.Panel>
